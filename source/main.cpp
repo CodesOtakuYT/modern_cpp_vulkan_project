b37b08aec3a2f4ef6a40abc218c6a106a656fba9
Codotaku <taouaouilyas@gmail.com>
2023-07-19 08:53:09 +0100

    Stream 3

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,24 @@
+#include <stdexcept>
+#include <expected>
+#include <ranges>
+
 #include <SDL.h>
 #include <SDL_vulkan.h>
-#include <stdexcept>
-#include <SDL_platform_defines.h>
+#include <SDL_syswm.h>
 
-#define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
-
-#ifdef __WINDOWS__
+#ifdef SDL_ENABLE_SYSWM_WINDOWS
 #define VK_USE_PLATFORM_WIN32_KHR
 #endif
+#ifdef SDL_ENABLE_SYSWM_WAYLAND
+#define VK_USE_PLATFORM_WAYLAND_KHR
+#endif
+#ifdef SDL_ENABLE_SYSWM_X11
+#define VK_USE_PLATFORM_XLIB_KHR
+#endif
+#define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
+#define VULKAN_HPP_NO_DEFAULT_DISPATCHER
 
 #include <vulkan/vulkan_raii.hpp>
-#include <SDL_syswm.h>
 
 class Noncopyable {
 public:
@@ -25,10 +33,6 @@ class SDLException : private std::runtime_error {
     const int code;
 public:
     explicit SDLException(const char *message, const int code = 0) : runtime_error(message), code{code} {}
-
-    [[nodiscard]] auto get_code() const noexcept {
-        return code;
-    }
 };
 
 class SDL : Noncopyable {
@@ -36,6 +40,10 @@ public:
     explicit SDL(SDL_InitFlags init_flags) {
         if (const auto error_code = SDL_Init(init_flags))
             throw SDLException{SDL_GetError(), error_code};
+    }
+
+    [[nodiscard]] auto get_platform() const {
+        return std::string_view{SDL_GetPlatform()};
     }
 
     ~SDL() {
@@ -55,9 +63,6 @@ public:
         SDL_Vulkan_UnloadLibrary();
     }
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "readability-convert-member-functions-to-static"
-
     [[nodiscard]] auto get_instance_proc_addr() const {
         if (const auto get_instance_proc_addr = reinterpret_cast<PFN_vkGetInstanceProcAddr>(SDL_Vulkan_GetVkGetInstanceProcAddr()))
             return get_instance_proc_addr;
@@ -74,8 +79,12 @@ public:
             throw SDLException{"Couldn't get vulkan instance extensions"};
         return extensions;
     }
+};
 
-#pragma clang diagnostic pop
+enum class PresentationSupportQueryError {
+    Failure,
+    Unknown,
+    Unimplemented,
 };
 
 class Window : Noncopyable {
@@ -92,6 +101,10 @@ public:
         SDL_DestroyWindow(handle);
     }
 
+    void show() const {
+        SDL_ShowWindow(handle);
+    }
+
     [[nodiscard]] auto create_surface(const vk::raii::Instance &instance) const {
         vk::SurfaceKHR::NativeType surface_handle;
         if (!SDL_Vulkan_CreateSurface(handle, *instance, &surface_handle))
@@ -99,63 +112,187 @@ public:
         return vk::raii::SurfaceKHR{instance, surface_handle};
     }
 
-    [[nodiscard]] auto get_physical_device_presentation_support(const vk::raii::PhysicalDevice &physical_device,
-                                                                uint32_t queue_family_index) const {
+    [[nodiscard]] std::expected<bool, PresentationSupportQueryError>
+    get_physical_device_presentation_support(const vk::raii::PhysicalDevice &physical_device,
+                                             uint32_t queue_family_index) const {
         SDL_SysWMinfo info;
         if (const auto return_code = SDL_GetWindowWMInfo(handle, &info, SDL_SYSWM_CURRENT_VERSION))
             throw SDLException{SDL_GetError(), return_code};
+        // https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap34.html#_querying_for_wsi_support
         switch (info.subsystem) {
+            case SDL_SYSWM_TYPE::SDL_SYSWM_ANDROID:
+            case SDL_SYSWM_TYPE::SDL_SYSWM_COCOA:
+            case SDL_SYSWM_TYPE::SDL_SYSWM_UIKIT:
+                return true;
+#ifdef VK_USE_PLATFORM_WIN32_KHR
             case SDL_SYSWM_TYPE::SDL_SYSWM_WINDOWS:
                 return static_cast<bool>(physical_device.getDispatcher()->vkGetPhysicalDeviceWin32PresentationSupportKHR(
                         *physical_device,
                         queue_family_index));
+#endif
+#ifdef VK_USE_PLATFORM_WAYLAND_KHR
+                case SDL_SYSWM_TYPE::SDL_SYSWM_WAYLAND:
+                    return static_cast<bool>(physical_device.getDispatcher()->vkGetPhysicalDeviceWaylandPresentationSupportKHR(
+                            *physical_device,
+                            queue_family_index, info.info.wl.display));
+#endif
+#ifdef VK_USE_PLATFORM_XLIB_KHR
+                case SDL_SYSWM_TYPE::SDL_SYSWM_X11: {
+                    XWindowAttributes attrib;
+                    const auto x11 = info.info.x11;
+                    if(X11_XGetWindowAttributes(x11.display, x11.window, &attrib)) {
+                        const auto visual = attrib.visual;
+                        const auto visual_id = X11_XVisualIDFromVisual(visual);
+                        return static_cast<bool>(physical_device.getDispatcher()->vkGetPhysicalDeviceXlibPresentationSupportKHR(
+                                *physical_device,
+                                queue_family_index, x11.display, visual_id));
+                    }
+                    return std::unexpected{PresentationSupportQueryError::Failure};
+                }
+#endif
+            case SDL_SYSWM_TYPE::SDL_SYSWM_UNKNOWN:
+                return std::unexpected{PresentationSupportQueryError::Unknown};
             default:
-                SDL_LogWarn(SDL_LogCategory::SDL_LOG_CATEGORY_APPLICATION,
-                            "Physical device presentation support for the current platform is not implemented, returning true anyway");
-                return true;
+                return std::unexpected{PresentationSupportQueryError::Unimplemented};
         }
-    }
-
-    [[nodiscard]] auto get_handle() const noexcept {
-        return handle;
     }
 };
 
 using QueueFamily = std::pair<vk::raii::PhysicalDevice, size_t>;
 
-auto main(int argc, char **argv) -> int {
+class Swapchain : Noncopyable {
+    vk::raii::SwapchainKHR handle;
+};
+
+class Surface : Noncopyable {
+    const vk::raii::SurfaceKHR handle;
+    std::optional<Swapchain> swapchain;
+    const QueueFamily &queue_family;
+public:
+    static auto create_swapchain(const vk::raii::Device &device, const QueueFamily &queue_family,
+                                 const vk::raii::SurfaceKHR &surface,
+                                 const std::optional<vk::SwapchainKHR> old_swapchain) {
+        const auto &[physical_device, queue_family_index] = queue_family;
+
+        const auto surface_capabilities{physical_device.getSurfaceCapabilitiesKHR(*surface)};
+        const auto surface_format{[&]() {
+            const auto surface_formats{physical_device.getSurfaceFormatsKHR(*surface)};
+            for (const auto &surface_format: surface_formats) {
+                if (surface_format.format == vk::Format::eB8G8R8A8Srgb &&
+                    surface_format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
+                    return surface_format;
+                }
+            }
+            return surface_formats.front();
+        }()};
+
+        vk::SwapchainCreateInfoKHR swapchain_create_info{};
+        swapchain_create_info.surface = *surface;
+        swapchain_create_info.clipped = true;
+        swapchain_create_info.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
+        swapchain_create_info.preTransform = surface_capabilities.currentTransform;
+        swapchain_create_info.imageSharingMode = vk::SharingMode::eExclusive;
+        swapchain_create_info.setQueueFamilyIndices(queue_family_index);
+        swapchain_create_info.presentMode = vk::PresentModeKHR::eFifo;
+        swapchain_create_info.imageExtent = surface_capabilities.currentExtent;
+        swapchain_create_info.minImageCount = std::clamp(surface_capabilities.minImageCount + 1,
+                                                         surface_capabilities.minImageCount,
+                                                         surface_capabilities.maxImageCount);
+        swapchain_create_info.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
+        swapchain_create_info.imageArrayLayers = 1;
+        swapchain_create_info.imageFormat = surface_format.format;
+        swapchain_create_info.imageColorSpace = surface_format.colorSpace;
+        if (old_swapchain.has_value())
+            swapchain_create_info.oldSwapchain = *old_swapchain;
+        return vk::raii::SwapchainKHR{device, swapchain_create_info};
+    }
+
+    Surface(const Window &window, const vk::raii::Instance &instance, const vk::raii::Device &device,
+            const QueueFamily &queue_family) : handle(
+            window.create_surface(instance)), queue_family{queue_family} {
+        create_swapchain(device, queue_family, handle, {});
+    }
+};
+
+auto main(const int, const char *const *const) -> int {
     const SDL sdl{SDL_InitFlags::SDL_INIT_VIDEO};
     const VulkanLibrary vulkan_library{};
 
-    vk::raii::Context context{vulkan_library.get_instance_proc_addr()};
+    const vk::raii::Context context{vulkan_library.get_instance_proc_addr()};
+    for (const auto &layer: context.enumerateInstanceLayerProperties()) {
+        SDL_Log("%s", layer.layerName.data());
+    }
 
     vk::ApplicationInfo application_info{};
     application_info.apiVersion = VK_API_VERSION_1_3;
 
     vk::InstanceCreateInfo create_info{};
     auto extensions = vulkan_library.get_instance_extensions();
-    if (SDL_GetPlatform() == std::string_view{"macOS"}) {
+#ifndef NDEBUG
+    extensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+#endif
+    if (sdl.get_platform() == "macOS") {
         create_info.flags |= vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR;
         extensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
     }
     create_info.setPEnabledExtensionNames(extensions);
     create_info.pApplicationInfo = &application_info;
-    const vk::raii::Instance instance{context, create_info};
 
-    const Window window{"Salam", 800, 600, SDL_WindowFlags::SDL_WINDOW_VULKAN};
-    const auto surface = window.create_surface(instance);
+#ifdef NDEBUG
+    vk::StructureChain<vk::InstanceCreateInfo> instance_structure_chain{create_info};
+#else
+    vk::DebugUtilsMessengerCreateInfoEXT debug_utils_messenger_create_info{};
+    debug_utils_messenger_create_info.messageSeverity =
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eError | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo | vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose;
+    debug_utils_messenger_create_info.messageType =
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+            vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eDeviceAddressBinding;
+    debug_utils_messenger_create_info.pfnUserCallback = [](
+            const VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+            const VkDebugUtilsMessageTypeFlagsEXT message_type,
+            const VkDebugUtilsMessengerCallbackDataEXT *const pCallbackData,
+            void *) -> VkBool32 {
+        const auto log{[](VkDebugUtilsMessageSeverityFlagBitsEXT message_severity) {
+            switch (message_severity) {
+                case VkDebugUtilsMessageSeverityFlagBitsEXT::VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+                    return SDL_LogError;
+                case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+                    return SDL_LogVerbose;
+                case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+                    return SDL_LogInfo;
+                case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+                    return SDL_LogWarn;
+                default:
+                    SDL_TriggerBreakpoint();
+                    std::unreachable();
+            }
+        }(message_severity)};
+        const auto message_type_string{vk::to_string(static_cast<vk::DebugUtilsMessageTypeFlagsEXT>(message_type))};
+        log(SDL_LogCategory::SDL_LOG_CATEGORY_APPLICATION, "{%s} %s: %s", pCallbackData->pMessageIdName,
+            message_type_string.data(),
+            pCallbackData->pMessage);
+        return vk::False;
+    };
+    vk::StructureChain<vk::InstanceCreateInfo, vk::DebugUtilsMessengerCreateInfoEXT> instance_structure_chain{
+            create_info, debug_utils_messenger_create_info};
+#endif
+
+    const vk::raii::Instance instance{context, instance_structure_chain.get<vk::InstanceCreateInfo>()};
+
+    const Window window{"Salam", 800, 600, static_cast<const SDL_WindowFlags>(SDL_WindowFlags::SDL_WINDOW_VULKAN |
+                                                                              SDL_WindowFlags::SDL_WINDOW_HIDDEN)};
 
     std::optional<QueueFamily> queue_family{};
     for (const auto &physical_device: instance.enumeratePhysicalDevices()) {
-        const auto queue_families_properties = physical_device.getQueueFamilyProperties();
-        for (std::size_t queue_family_index{};
-             queue_family_index != queue_families_properties.size(); ++queue_family_index) {
-            const auto queue_family_properties{queue_families_properties[queue_family_index]};
+        for (const auto &[queue_family_index, queue_family_properties]: std::views::zip(std::views::iota(0),
+                                                                                        physical_device.getQueueFamilyProperties())) {
             const auto does_support_graphics{queue_family_properties.queueFlags & vk::QueueFlagBits::eGraphics};
             const auto does_support_presentation{
                     window.get_physical_device_presentation_support(physical_device, queue_family_index)};
 
-            if (does_support_graphics && does_support_presentation)
+            if (does_support_graphics && does_support_presentation.value_or(true))
                 queue_family = {physical_device, queue_family_index};
         }
     }
@@ -164,23 +301,40 @@ auto main(int argc, char **argv) -> int {
     const auto &[physical_device, queue_family_index] = *queue_family;
     vk::DeviceCreateInfo info{};
     vk::DeviceQueueCreateInfo queue_create_infos{};
-    const auto queue_priorities{0.0f};
+    const auto queue_priorities = {0.0f};
     queue_create_infos.setQueuePriorities(queue_priorities);
     queue_create_infos.queueFamilyIndex = queue_family_index;
     info.setQueueCreateInfos(queue_create_infos);
+    const auto device_extensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+    info.setPEnabledExtensionNames(device_extensions);
 
     const vk::raii::Device device{physical_device, info};
     const vk::raii::Queue queue{device, queue_family_index, 0};
 
-    bool should_close{};
-    while (!should_close) {
+    std::optional<Surface> surface{};
+    const auto create_surface = [&]() {
+        surface.emplace(window, instance, device, *queue_family);
+    };
+#ifndef __ANDROID__
+    create_surface();
+#endif
+    window.show();
+
+    for (auto should_close{false}; !should_close;) {
         for (SDL_Event event; SDL_PollEvent(&event);) {
             switch (event.type) {
                 case SDL_EventType::SDL_EVENT_QUIT:
                     should_close = true;
+                    break;
+                case SDL_EventType::SDL_EVENT_DID_ENTER_FOREGROUND:
+                    create_surface();
+                    break;
+                case SDL_EventType::SDL_EVENT_WILL_ENTER_BACKGROUND:
+                    surface.reset();
+                    break;
             }
         }
     }
 
     return 0;
-};
+}
